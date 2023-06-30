30cfcb3016f7ab7225679c452ea08d9c9b1e7551
Viktor Miric <viktorm47@gmail.com>
2023-06-30 11:56:27 +0200

    Styling for posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
-  validates :body, length: { minimum: 8 }
+  validates :body, length: { minimum: 4 }
 end
