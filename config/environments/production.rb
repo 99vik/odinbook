22ec91a940b402d4b099eaf6b997e541970db7a4
Viktor Miric <viktorm47@gmail.com>
2023-07-10 22:18:08 +0200

    Adding devise config for password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { :host => 'rails-23rb.onrender.com', :protocol => 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
