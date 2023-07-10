98bbc58b7c5e86fb8535be17fbf065687cd2a76f
Viktor Miric <viktorm47@gmail.com>
2023-07-10 18:24:29 +0200

    Production modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Friendship.destroy_all
+Notification.destroy_all
+User.destroy_all
+
 def create_users
   User.create(id: 1, username: "John Doe", email: "johndoe@mail.com", password: "password")
   User.create(id: 2, username: "Jane Doe", email: "janedoe@mail.com", password: "password")
